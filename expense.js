a8b6b516c44da743f1d4e92b8afc28c7ca5fd193
Tydomx <michaelthai16@gmail.com>
2022-09-15 20:24:28 -0500

    Added CSS file, images, html format

--- expense.js
+++ expense.js
@@ -1,5 +1,34 @@
-// functionality for displaying list of expenses
+const amountInput = document.getElementById('number');
+const addForm = document.getElementById('addForm');
 
+const budgetAmount = document.getElementById('budgetAmount');
+const balanceAmount = document.getElementById('balanceAmount');
+
+function getBudgetAmount(amount) {
+	if (!amount) {
+		amountInput.style.border = '1px solid #b80c09';
+		amountInput.placeholder = 'input can not be empty';
+		amountInput.style.color = '#b80c09';
+		setTimeout(() => {
+			amountInput.style.color = '#495057';
+			amountInput.style.border = '1px solid gray';
+		}, 3000);
+	} else {
+		budgetAmount.innerText = amount;
+		balanceAmount.innerText = amount;
+		expenseForm.style.display = 'block';
+		budgetForm.style.display = 'none';
+		editForm.style.display = 'none';
+		amountInput.value = '';
+	}
+}
+
+addForm.addEventListener('submit', (e) => {
+	e.preventDefault();
+	getBudgetAmount(amountInput.value);
+});
+
+// functionality for displaying list of expenses
 const expForm = document.getElementById('expForm');
 let expName = document.getElementById('expName');
 let expNumber = document.getElementById('expNumber');
@@ -41,6 +70,7 @@ function addExpenses(name, number) {
 };
 
 // function to display expense list after user adds expense
+// takes array as parameter and inside function, loop thru array using for loop and user innerHTML append object to HTML tag
 function displayExp(details) {
 	expValue.innerHTML = null;
 	for (i = 0; i < details.length; i++) {
